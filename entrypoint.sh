ec6b4eb885d7737e5cf69438975f339396b05108
saisona <alexandre.saison.pro@gmail.com>
2021-06-01 19:53:00 +0200

    Update: use variable LOKI_FILE_LOCATION_ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,6 @@ else
 fi
 
 MODEL_INSTANCE_ID="$INPUT_MODELINSTANCEID"
-MODEL_FILELOC_ID="$NUTSHELL_WORKER_MODEL_FILE_LOC_ID"
 WORFLOW_TEMPLATE_NAME="$INPUT_WORKFLOWTEMPLATENAME"
 REGRESSION_TEST_ID="non_regression_$MODEL_INSTANCE_ID_$(date +"%s")"
 ARGO_SERVER="$INPUT_ARGOSERVER"
@@ -32,12 +31,6 @@ then
   echo "[$(date +"%m/%d/%y %T")] Error: missing ARGO_TOKEN env variable"
   exit 1
 
-elif [[ -z $MODEL_FILELOC_ID ]]
-then
-
-  echo "[$(date +"%m/%d/%y %T")] Error: missing MODEL_FILELOC_ID env variable"
-  exit 1
-
 elif [[ -z $WORFLOW_TEMPLATE_NAME ]]
 then
 
@@ -52,5 +45,5 @@ echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_id :  $REGRESSION_TEST_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p model_instance_id:  $MODEL_INSTANCE_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_file_location_id:  $MODEL_FILELOC_ID"
 
-RESPONSE=$(argo submit -s $ARGO_SERVER --token $ARGO_TOKEN --from $WORFLOW_TEMPLATE_NAME  -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$MODEL_FILELOC_ID)
+RESPONSE=$(argo submit -s $ARGO_SERVER --token $ARGO_TOKEN --from $WORFLOW_TEMPLATE_NAME  -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID)
 
