4c9f845cdf4ae28e13553796d09b3bd661d1e7e9
saisona <alexandre.saison.pro@gmail.com>
2021-06-07 22:25:52 +0200

    Update: handle new updates
     -> using new workflow parameters when using staging env
     -> adding new debug logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ then
   export $(grep -v '^#' .env | xargs)
   echo "[$(date +"%m/%d/%y %T")] Exported all env variables"
 else 
-  echo "[$(date +"%m/%d/%y %T")] An error occured during import .env variables"
+  echo "[$(date +"%m/%d/%y %T")] An error occured during import .env variables (Reason: no .env file found)"
   exit 1
 fi
 
@@ -12,10 +12,15 @@ fi
 echo "Creation of the KUBECONFIG and connection to the cluster $CLUSTER_NAME"
 aws eks --region eu-west-1 update-kubeconfig --name $CLUSTER_NAME
 
+if [[ $? == 1 ]]
+then
+  echo "[$(date +"%m/%d/%y %T")] An error occured during creation of kubeconfig or connection to cluster $CLUSTER_NAME"
+  exit
+fi
+
 MODEL_INSTANCE_ID="$INPUT_MODELINSTANCEID"
 WORFLOW_TEMPLATE_NAME="$INPUT_WORKFLOWTEMPLATENAME"
 REGRESSION_TEST_ID="non-regression-${MODEL_INSTANCE_ID}-$(date +"%s")"
-ARGO_SERVER="$INPUT_ARGOSERVER"
 
 if [[ -z $MODEL_INSTANCE_ID ]]
 then
@@ -33,10 +38,11 @@ fi
 
 echo "::group::Argo arguments list"
 echo "[$(date +"%m/%d/%y %T")] Launching Loki workflow $WORFLOW_TEMPLATE_NAME"
-echo "[$(date +"%m/%d/%y %T")] Arguments are -s :  $ARGO_SERVER"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_id :  $REGRESSION_TEST_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p model_instance_id:  $MODEL_INSTANCE_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_file_location_id:  $LOKI_FILE_LOCATION_ID"
+echo "[$(date +"%m/%d/%y %T")] Arguments are -p environment: $WORKER_ENV"
+
 echo "::endgroup::"
 
 echo "Fetching list of current Argo Workflow."
@@ -45,8 +51,24 @@ argo list
 echo "::endgroup::"
 
 echo "::group::Launching ArgoWorkflow"
-echo "Launching argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID"
-WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w  -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -o json | jq -e -r .metadata.name)
+echo "[$(date +"%m/%d/%y %T")] Launching argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID"
+echo "[$(date +"%m/%d/%y %T")] Using $WORKER_ENV environment"
+
+if [[ $WORKER_ENV == "staging" ]]
+then
+  local API_ENDPOINT="staging.api.inarix.com"
+  local PREDICTION_ENDPOINT="https://${API_ENDPOINT}/imodels/predict"
+  echo "[$(date +"%m/%d/%y %T")] Adding arguments inarix_api_hostname: $API_ENDPOINT"
+  echo "[$(date +"%m/%d/%y %T")] Adding arguments prediction_entrypoint: $PREDICTION_ENDPOINT"
+  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -p inarix_api_hostname=$API_ENDPOINT -p prediction_entrypoint=$PREDICTION_ENDPOINT  -o json | jq -e -r .metadata.name)
+elif [[ $WORKER_ENV == "production"]]
+then
+  echo "[$(date +"%m/%d/%y %T")] When using env $WORKER_ENV inarix_api_hostname and prediction_entrypoint used are the default ones (created for production purpose)"
+  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -o json | jq -e -r .metadata.name)
+else
+  echo "[$(date +"%m/%d/%y %T")] Error: missing WORFLOW_TEMPLATE_NAME env variable"
+  exit 1
+fi
 echo "::endgroup::"
 
 # -- Get Worflow metadata --
