046905ca827ed1c3ba6412b88255618c4aa254a9
saisona <alexandre.saison.pro@gmail.com>
2021-06-15 16:49:30 +0200

    Update: some updates for ci testings

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,6 @@ echo "[$(date +"%m/%d/%y %T")] Launching Loki workflow $WORFLOW_TEMPLATE_NAME"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_id :  $REGRESSION_TEST_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p model_instance_id:  $MODEL_INSTANCE_ID"
 echo "[$(date +"%m/%d/%y %T")] Arguments are -p test_file_location_id:  $LOKI_FILE_LOCATION_ID"
-echo "[$(date +"%m/%d/%y %T")] Arguments are -p environment: $WORKER_ENV"
 echo "::endgroup::"
 
 echo "::group::List Workflows"
@@ -50,23 +49,7 @@ echo "::endgroup::"
 
 echo "::group::Launching ArgoWorkflow"
 echo "[$(date +"%m/%d/%y %T")] Launching argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID"
-echo "[$(date +"%m/%d/%y %T")] Using $WORKER_ENV environment"
-
-if [[ $WORKER_ENV == "staging" ]]
-then
-  API_ENDPOINT="staging.api.inarix.com"
-  PREDICTION_ENDPOINT="https://${API_ENDPOINT}/imodels/predict"
-  echo "[$(date +"%m/%d/%y %T")] Adding arguments inarix_api_hostname: $API_ENDPOINT"
-  echo "[$(date +"%m/%d/%y %T")] Adding arguments prediction_entrypoint: $PREDICTION_ENDPOINT"
-  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -p inarix_api_hostname=$API_ENDPOINT -p prediction_entrypoint=$PREDICTION_ENDPOINT  -o json | jq -e -r .metadata.name)
-elif [[ $WORKER_ENV == "production" ]]
-then
-  echo "[$(date +"%m/%d/%y %T")] When using env $WORKER_ENV inarix_api_hostname and prediction_entrypoint used are the default ones (created for production purpose)"
-  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -o json | jq -e -r .metadata.name)
-else
-  echo "[$(date +"%m/%d/%y %T")] Error: $WORKER_ENV must be one of [staging, production]"
-  exit 1
-fi
+WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -o json | jq -e -r .metadata.name)
 echo "::endgroup::"
 
 # -- Get Worflow metadata --
@@ -89,8 +72,6 @@ HAS_SUCCEED=$(echo -n "${LOGS}" | tail -n1 | cut -d : -f2- | tr -d ' ')
 
 # -- Remove line breaker before sending values (Required by GithubAction)  --
 LOGS="${LOGS//$'\n'/'%0A'}"
-
-echo "HAS_SUCCEED=$HAS_SUCCEED"
 
 if [[ "${HAS_SUCCEED}" == "TEST_HAS_FAILED" ]]
 then
