9d20eb255fa42a393042ef442f26edcadce4cec5
saisona <alexandre.saison.pro@gmail.com>
2021-06-17 20:43:32 +0200

    Update: revert changes
    -> BACK TO 2 DIFFERENT WORKER_ENV

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,21 @@ echo "::endgroup::"
 
 echo "::group::Launching ArgoWorkflow"
 echo "[$(date +"%m/%d/%y %T")] Launching argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID"
-WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -o json | jq -e -r .metadata.name)
+if [[ $WORKER_ENV == "staging" ]]
+then
+  API_ENDPOINT="staging.api.inarix.com"
+  PREDICTION_ENDPOINT="https://${API_ENDPOINT}/imodels/predict"
+  echo "[$(date +"%m/%d/%y %T")] Adding arguments inarix_api_hostname: $API_ENDPOINT"
+  echo "[$(date +"%m/%d/%y %T")] Adding arguments prediction_entrypoint: $PREDICTION_ENDPOINT"
+  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -p environment=$WORKER_ENV -p inarix_api_hostname=$API_ENDPOINT -p prediction_entrypoint=$PREDICTION_ENDPOINT  -o json | jq -e -r .metadata.name)
+elif [[ $WORKER_ENV == "production" ]]
+then
+  echo "[$(date +"%m/%d/%y %T")] When using env $WORKER_ENV inarix_api_hostname and prediction_entrypoint used are the default ones (created for production purpose)"
+  WORKFLOW_NAME=$(argo submit --from $WORFLOW_TEMPLATE_NAME -w -p test_id=$REGRESSION_TEST_ID -p model_instance_id=$MODEL_INSTANCE_ID -p test_file_location_id=$LOKI_FILE_LOCATION_ID -o json | jq -e -r .metadata.name)
+else
+  echo "[$(date +"%m/%d/%y %T")] Error: $WORKER_ENV must be one of [staging, production]"
+  exit 1
+fi
 echo "::endgroup::"
 
 # -- Get Worflow metadata --
